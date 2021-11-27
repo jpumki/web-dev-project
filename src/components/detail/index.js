8f07fde51fa1bb432491e4027526905b9aa82c5f
junho-wk <64652497+junho-wk@users.noreply.github.com>
2021-11-27 14:50:59 -0500

    update

--- src/components/detail/index.js
+++ src/components/detail/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from "react";
 import { moviesApi, tvApi } from "../../api";
-
+import { useParams } from "react-router-dom";
+import "./detail.css";
+import popcorn from "../../assets/noPosterSmall.png";
 const Detail = () => {
   const [result, setResult] = useState();
   const [isMovie, setIsMovie] = useState(
@@ -9,39 +11,88 @@ const Detail = () => {
   const [error, setError] = useState();
   const [loading, setLoading] = useState();
 
-  // useEffect(() => {
-  //   async function getFilmDetail() {
-  //     const {
-  //       match: {
-  //         params: { id },
-  //       },
-  //       history: { push },
-  //     } = this.props;
-  //     const { isMovie } = this.state;
-  //     const parsedId = parseInt(id);
-  //     if (isNaN(parsedId)) {
-  //       return push("/");
-  //     }
-  //     let result = null;
-  //     try {
-  //       if (isMovie) {
-  //         ({ data: result } = await moviesApi.movieDetail(parsedId));
-  //       } else {
-  //         ({ data: result } = await tvApi.showDetail(parsedId));
-  //       }
-  //       setResult(result);
-  //     } catch {
-  //       setError("Can't find anything.");
-  //     } finally {
-  //       setLoading(false);
-  //       setResult(result);
-  //     }
-  //   }
-  //   console.log(window.location.pathname);
-  //   getFilmDetail();
-  // }, []);
+  const { id } = useParams();
+  useEffect(() => {
+    async function getFilmDetail() {
+      const parsedId = parseInt(id);
+      // if (isNaN(parsedId)) {
+      //   window.location.href = "/";
+      // }
+      let result = null;
+      try {
+        if (isMovie) {
+          ({ data: result } = await moviesApi.movieDetail(parsedId));
+        } else {
+          ({ data: result } = await tvApi.showDetail(parsedId));
+        }
+        setResult(result);
+      } catch {
+        setError("Can't find anything.");
+      } finally {
+        setLoading(false);
+        setResult(result);
+      }
+    }
+    getFilmDetail();
+  }, []);
 
-  return <div>{console.log(isMovie)}</div>;
+  return (
+    loading == false && (
+      <div className="detail-container">
+        <div
+          className="detail-backdrop"
+          style={{
+            backgroundImage: result.backdrop_path
+              ? `url(https://image.tmdb.org/t/p/original${result.backdrop_path})`
+              : `url(${popcorn})`,
+          }}
+        />
+        <div className="detail-content">
+          <div
+            className="detail-cover"
+            style={{
+              backgroundImage: result.poster_path
+                ? `url(https://image.tmdb.org/t/p/original${result.poster_path})`
+                : `url(${popcorn})`,
+            }}
+          />
+          <div className="detail-data">
+            <h3 className="detail-title">
+              {result.original_title
+                ? result.original_title
+                : result.original_name}
+            </h3>
+            <div className="detail-itemcontainer">
+              <span className="detail-item">
+                {result.release_date
+                  ? result.release_date.substring(0, 4)
+                  : result.first_air_date.substring(0, 4)}
+              </span>
+              <span className="detail-divider">•</span>
+              <span className="detail-item">
+                {result.runtime ? result.runtime : result.episode_run_time[0]}{" "}
+                min
+              </span>
+              <span className="detail-divider">•</span>
+              <span className="detail-item">
+                {result.genres &&
+                  result.genres.map((genre, index) =>
+                    index === result.genres.length - 1
+                      ? genre.name
+                      : `${genre.name} / `
+                  )}
+              </span>
+              <span className="detail-divider">•</span>
+              <span className="detail-item">
+                {result.vote_average ? `⭐️${result.vote_average}/10` : ``}
+              </span>
+            </div>
+            <p className="detail-overview">{result.overview}</p>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default Detail;
