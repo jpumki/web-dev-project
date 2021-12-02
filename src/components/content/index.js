d57fc1e50dc2699dd6a0d1caa9d21235b3be4c85
junho-wk <64652497+junho-wk@users.noreply.github.com>
2021-12-02 13:58:05 -0500

    Update index.js

--- src/components/content/index.js
+++ src/components/content/index.js
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Search from "../search";
 import Home from "../home";
 import Detail from "../detail"
+import Profile from "../profile";
 const Contents = () => {
   return (
     <Router>
@@ -14,6 +15,7 @@ const Contents = () => {
           <Route path="/search" element={<Search />} />
           <Route path="/movie/:id" element={<Detail />} />
           <Route path="/show/:id" element={<Detail />} />
+          <Route path="/profile/:id" element={<Profile />} />
         </Routes>
       </>
     </Router>
