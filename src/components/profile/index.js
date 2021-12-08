ee1cc16b78a83d7a65d9a4c0abe602998ed92b91
junho-wk <64652497+junho-wk@users.noreply.github.com>
2021-12-08 13:45:17 -0500

    update

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import "./profile.css";
 import service from "../../service/service";
 import { useParams } from "react-router-dom";
+import popcorn from "../../assets/noPosterSmall.png";
 const Profile = ({ auth }) => {
   const [profile, setProfile] = useState();
+  const [init, setInit] = useState(false);
 
   const findProfileById = (id) => {
     service.findProfileById(id).then((profile) => {
@@ -11,6 +13,7 @@ const Profile = ({ auth }) => {
         window.location.href = "/";
       } else {
         setProfile(profile);
+        setInit(true);
       }
     });
   };
@@ -30,72 +33,206 @@ const Profile = ({ auth }) => {
   }, []);
 
   return (
-    <div>
-      <div>
-        {console.log(profile)}
-        <div>
-          <img />
-        </div>
-        <span>junhopark</span>
-        <img />
-        <span>member since 2021</span>
-      </div>
-      <div>
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
-            <a class="nav-link active">Followers</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">Following</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link">Favorites</a>
-          </li>
-        </ul>
-      </div>
-      <AccountDetail />
-      <hr />
-      <PlanDetail />
-      <hr />
-      <Settings />
+    <div style={{ margin: "60px" }}>
+      {init && (
+        <>
+          <div>
+            <div>
+              <img
+                style={{ width: "70px", height: "70px", borderRadius: "50%" }}
+                src={
+                  "https://th.bing.com/th/id/R.1abee17234dca9feaf9d0064bf491f6e?rik=Gs1TTKs9hDMyHg&pid=ImgRaw&r=0"
+                }
+              />
+              <spacer /> <spacer />
+              <span style={{ fontSize: "20px" }}>{profile.name}</span>
+              <img
+                style={{ width: "35px", height: "30px", marginLeft: "30px" }}
+                src={
+                  "https://myhoneypotsjuicy.com/wp-content/uploads/2018/05/video-icon.png"
+                }
+              />
+              <spacer /> <spacer /> Member since {profile.date.substring(0, 4)}
+            </div>
+          </div>
+
+          <div id="container">
+            <input id="tab-1" type="radio" name="tab-group" checked="checked" />
+            <label htmlFor="tab-1">Followers</label>
+
+            <input id="tab-2" type="radio" name="tab-group" />
+            <label htmlFor="tab-2">Followings</label>
+
+            <input id="tab-3" type="radio" name="tab-group" />
+            <label htmlFor="tab-3">Favorites</label>
+
+            <div id="content">
+              <div id="content-1">
+                <p className="container">
+                  <p className="card" style={{ float: "left" }}>
+                    <img
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        borderRadius: "50%",
+                        margin: "auto auto 15px",
+                        display: "block",
+                      }}
+                      src="https://i.pinimg.com/originals/e2/c7/ba/e2c7ba0ef1467ac03b077275e14a9247.jpg"
+                    />
+                    <p>Kskathy</p>
+                    <p style={{ color: "gray" }}>Profile</p>
+                  </p>
+                </p>
+
+                <p className="container">
+                  <p className="card" style={{ marginLeft: "150px" }}>
+                    <img
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        borderRadius: "50%",
+                        margin: "auto auto 15px",
+                        display: "block",
+                      }}
+                      src="https://th.bing.com/th/id/OIP.zONx8yK7LpWE15PpDQ-a9AAAAA?pid=ImgDet&rs=1"
+                    />
+                    <p>TaylorSwift</p>
+                    <p style={{ color: "gray" }}>Profile</p>
+                  </p>
+                </p>
+              </div>
+
+              <div id="content-2">
+                <p className="container">
+                  <p className="card" style={{ float: "left" }}>
+                    <img
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        borderRadius: "50%",
+                        margin: "auto auto 15px",
+                        display: "block",
+                      }}
+                      src="https://th.bing.com/th/id/OIP.ulawi7jK0U9slojC8fSNJQHaFj?pid=ImgDet&rs=1"
+                    />
+                    <p>KimKardashian</p>
+                    <p style={{ color: "gray" }}>Profile</p>
+                  </p>
+                </p>
+
+                <p className="container">
+                  <p className="card" style={{ marginLeft: "150px" }}>
+                    <img
+                      style={{
+                        height: "70px",
+                        width: "70px",
+                        borderRadius: "50%",
+                        margin: "auto auto 15px",
+                        display: "block",
+                      }}
+                      src="https://th.bing.com/th/id/R.5288ec5b89cac92fc7877fda637f5188?rik=XiD72loz%2fDFPoA&pid=ImgRaw&r=0"
+                    />
+                    <p>JakeJ</p>
+                    <p style={{ color: "gray" }}>Profile</p>
+                  </p>
+                </p>
+              </div>
+
+              <div id="content-3">
+                {profile.movieList.length > 0 && (
+                  <ul>
+                    {profile.movieList.map((elem) => {
+                      return (
+                        <li className="mx-2 w-100 cursor-pointer">
+                          <a href={elem.type === 0 ? `/movie/${elem.id}` : `/show/${elem.id}`}>
+                            <p className="container">
+                              <img
+                                style={{
+                                  width: "100px",
+                                  height: "150px",
+                                  borderRadius: "10px",
+                                }}
+                                src={
+                                  elem.img
+                                    ? `https://image.tmdb.org/t/p/w300${elem.img}`
+                                    : popcorn
+                                }
+                              />
+                              <p>{elem.name}</p>
+                              <p style={{ color: "gray" }}>
+                                {elem.year.substring(0, 4)}
+                              </p>
+                            </p>
+                          </a>
+                        </li>
+                      );
+                    })}
+                  </ul>
+                )}
+              </div>
+            </div>
+          </div>
+
+          <AccountDetail profile={profile} />
+          <hr />
+          <PlanDetail />
+          <hr />
+          <Settings />
+        </>
+      )}
     </div>
   );
 };
 
-const AccountDetail = () => {
+const AccountDetail = ({ profile }) => {
   return (
-    <div>
-      <h3 className="pb-3">Account</h3>
+    <div style={{ marginTop: "40px" }}>
+      <h3 className="pb-3" style={{ fontSize: "20px" }}>
+        Account
+      </h3>
       <hr className="mb-3" />
       <div>
         <div className="d-flex">
           <div className="col-xl-3">
             <div clssName="justify-contents-center align-items-center">
               <div>Membership & Billing</div>
-              <button className="btn">Cancel Membership</button>
+              <button
+                className="btnClass"
+                style={{
+                  width: "120px",
+                  marginTop: "20px",
+                  color: "black",
+                  backgroundColor: "gray",
+                }}
+              >
+                Cancel Membership
+              </button>
             </div>
           </div>
           <div className="col-xl-9">
             <div className="profile-account-row">
-              <span>park.junho@gmail.com</span>
-              <a>Change account email</a>
-            </div>{" "}
-            <div className="profile-account-row">
-              <span>password : ******</span>
-              <a>Change password</a>
+              <span>{profile.email}</span>
+              <a style={{ color: "blue" }}>Change account email</a>
             </div>
             <div className="profile-account-row">
-              <span>Phone : (646) 234 - 4234</span>
-              <a>Change phone number</a>
+              <span style={{ color: "gray" }}>password : ******</span>
+              <a style={{ color: "blue" }}>Change password</a>
+            </div>
+            <div className="profile-account-row">
+              <span style={{ color: "gray" }}>Phone : (646) 234 - 4234</span>
+              <a style={{ color: "blue" }}>Change phone number</a>
             </div>
             <hr className="mb-3" />
             <div className="profile-account-row">
               <span>junhoparkspaypal@gmail.com</span>
-              <a>Manage payment info</a>
+              <a style={{ color: "blue" }}>Manage payment info</a>
             </div>
             <div className="profile-account-row">
-              <span>Your next billing date is December 20, 2021</span>
-              <a>Billing details</a>
+              <span style={{ color: "gray" }}>
+                Your next billing date is December 20, 2021
+              </span>
+              <a style={{ color: "blue" }}>Billing details</a>
             </div>
           </div>
         </div>
@@ -110,7 +247,7 @@ const PlanDetail = () => {
       <div className="col-xl-3">Plan Detail</div>
       <div className="d-flex justify-content-between col-xl-9">
         <span>Premium</span>
-        <a>Change Plan</a>
+        <a style={{ color: "blue" }}>Change Plan</a>
       </div>
     </div>
   );
@@ -129,7 +266,7 @@ const Settings = () => {
         {setting.map((elem) => {
           return (
             <div>
-              <a>{elem}</a>
+              <a style={{ color: "blue" }}>{elem}</a>
             </div>
           );
         })}
